1cdbcc4879a6468b52649e9324de8241c58c6aeb
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-05-09 00:04:35 +0530

    3_Digit_ArmstrongNumber

---
+++ Foundation/AmstrongNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AmstrongNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int N = input.nextInt();
+
+        int Original = N;
+        int Sum = 0;
+        while (N > 0) {
+            int digit = N % 10;
+            Sum = Sum + (digit*digit*digit);
+            N = N/10;
+        }
+        if(Original == Sum){
+            System.out.println("The Given Number is Armstrong ");
+        }else{
+            System.out.println("The Given Number is Not an Arsmstrong ");
+        }
+    }
+}
