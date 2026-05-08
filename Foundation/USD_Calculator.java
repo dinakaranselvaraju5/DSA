b04a9a87963c79c76a1c56078fe9530c3d759655
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-05-08 21:50:11 +0530

    USD_Calculator

---
+++ Foundation/USD_Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class USD_Calculator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a INR Amount : ");
+        double INR = input.nextDouble();
+        input.close();
+        double USD = (INR/94.40);
+        System.out.printf("USD is %.2f" ,USD);
+    }
+}
