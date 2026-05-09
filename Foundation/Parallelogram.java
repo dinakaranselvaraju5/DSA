f17fcaf73b4950a32a61452a5839a5d39265b222
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-05-10 01:16:13 +0530

    Area of Parallelogram

---
+++ Foundation/Parallelogram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Parallelogram {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // d1 and d2 are Diagonals
+        System.out.print("Enter a Diagonal 1 : ");
+        int d1 = input.nextInt();
+        System.out.print("Enter a Diagonal 2 : ");
+        int d2 = input.nextInt();
+        input.close();
+
+        double Area = 0.5 * d1 *d2;
+        System.out.println(Area);
+    }
+}
