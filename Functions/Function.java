37464339aff8ea821fb7e4413834d081ef7617c1
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-05-12 21:35:32 +0530

    Functions

---
+++ Functions/Function.java
@@ -0,0 +1,51 @@
+package Functions;
+
+import java.util.*;
+ public class Function {
+
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Sum();
+        int Answer = Subract();
+        System.out.println("Subract : " + Answer);
+        int Ans = Multiply(10, 20);
+        System.out.println("Multiply : " + Ans);
+          input.close();
+    }
+
+
+    // Everything inside a Function 
+
+     static void Sum(){
+        
+        System.out.print("Enter a Number 1 : ");
+        int N1 = input.nextInt();
+        System.out.print("Enter a Number 2 : ");
+        int N2 = input.nextInt();
+      
+        int Ans = N1 + N2;
+        System.out.println("Addition : " + Ans);
+        
+
+    }  
+
+    // Using int to return the value
+
+        static int Subract (){
+            
+        System.out.print("Enter a Number 1 : ");
+        int N1 = input.nextInt();
+        System.out.print("Enter a Number 2 : ");
+        int N2 = input.nextInt();
+        int Ans = N1 - N2;
+        return Ans;
+        }
+
+        // Using Paramters
+        static int Multiply(int A, int B){
+            int ans = A * B;
+            return ans;
+        }
+       
+    
+}
