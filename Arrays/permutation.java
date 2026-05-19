6cc4b5415defbdec6fe51a5654071261e34472b3
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-05-19 17:53:03 +0530

    permutation

---
+++ Arrays/permutation.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class permutation {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = {0,2,1,5,3,4};
+        
+       int[] ans = new int[arr.length];
+       for(int i=0;i<arr.length;i++){
+         ans[i] = arr[arr[i]];
+       } 
+
+       System.out.println(Arrays.toString(ans));
+              
+              
+             
+            
+        }
+       // System.out.println(Arrays.toString(arr));
+
+        //System.out.println(Arrays.toString(arr));
+    }
