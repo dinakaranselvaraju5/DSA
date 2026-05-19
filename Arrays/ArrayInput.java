798eaacde1672bb8c097785e54584a7d7abcb133
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-05-19 08:52:14 +0530

    Array's Input from User

---
+++ Arrays/ArrayInput.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class ArrayInput{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Take input in Loop
+        int[] arr = new int[5];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = input.nextInt();
+        }
+
+       // Printting the Array's
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i] + " ");
+        // }
+
+        // For Each loop
+        for (int Nums : arr) {
+            System.out.print (Nums + " ");
+            
+        }
+        System.out.println();
+        System.out.println (Arrays.toString(arr));
+    }
+}
