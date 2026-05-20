be05cc78407ce560ac5cca171f04ec7d2a5ca0cd
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-05-20 22:21:59 +0530

    ArrayList Basics

---
+++ Arrays/AList.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.ArrayList;
+import java.util.*;
+public class AList {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        ArrayList<Integer> list2 = new ArrayList<>(list);
+        list2.addAll(list);
+        System.out.println(list2);
+        list2.add(1,100);
+        System.out.println(list2);
+        list2.addFirst(1);
+        System.out.println(list2);
+        list2.remove(0);
+        System.out.println(list2);
+        list2.contains(400);
+        System.out.println(list2);
+    }
+}
