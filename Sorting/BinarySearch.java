3247b432eebf1b43e7c7bdf1f664807083963efb
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-06-01 15:09:55 +0530

    BinarySearch

---
+++ Sorting/BinarySearch.java
@@ -0,0 +1,27 @@
+package Sorting;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1,4,9000,900000};
+        int target = 9000;
+        int Ans = Searching(arr,target);
+       System.out.println("Index of the target : " + Ans);
+    }
+
+    private static int Searching(int[] arr, int target) {
+       int left = 0;
+       int right = arr.length - 1;
+       while (left<=right) {
+            int mid = left + (right - left)/2;
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+       }  
+       
+       return -1;
+    }
+}
