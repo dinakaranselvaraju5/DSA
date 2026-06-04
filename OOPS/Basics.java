4be8ffe1ff3bcd1daf69b068aa1718429ecb54db
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-06-04 18:18:35 +0530

    Types of Constructors in Java

--- OOPS/Basics.java
+++ OOPS/Basics.java
@@ -5,6 +5,8 @@ public class Basics {
     public static void main(String[] args) {
         A obj = new A();
         System.out.println(obj);
+        Constructor c = new Constructor();
+        c.Greet();
 
     }
     static class A{
