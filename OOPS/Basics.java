c7577f34603328e6075f3ad55413614d88702eab
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-06-04 17:54:46 +0530

    Default Value

--- OOPS/Basics.java
+++ OOPS/Basics.java
@@ -3,8 +3,17 @@ package OOPS;
 public class Basics {
 
     public static void main(String[] args) {
+        A obj = new A();
+        System.out.println(obj);
+
+    }
+    static class A{
+        int a;
+        String Name;
+        A(){
+
+        }
         
     }
-
 }
 
