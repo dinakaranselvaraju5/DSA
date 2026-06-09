57f4dc0a4586e52fb862268fa48efdc5c83318b6
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-06-09 14:01:20 +0530

    RemoveDuplicates using Two Pointers

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class RemoveDuplicates {
 
     public static void main(String[] args) {
@@ -7,14 +9,17 @@ public class RemoveDuplicates {
         // Using Two Pointer Pattern
 
         int[] nums = {1,1,1,2,2,2,3,4,4,4,4,4,4,4,4,4,6};
-
+         
         int i = 0;
         for(int j = 1;j<nums.length;j++){
             if(nums[i] != nums[j]){
                 nums[i + 1] = nums[j];
+                
                 i++;
             }
         }
+        
+        //System.out.println(Arrays.toString(Answer));
 
         System.out.println(i + 1);
     }
