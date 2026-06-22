4289e949e79f6fc0b12e312c32be7a4f0949aed1
DINAKARAN SELVARAJU <dinakaranselvaraju5@gmail.com>
2026-06-22 12:24:27 +0530

    Divisible and Non-divisible Sums Difference

---
+++ Leetcode_Series/SumDifference.java
@@ -0,0 +1,50 @@
+/*
+
+        Leetcode Question Number : 2894
+        Divisible and Non-divisible Sums Difference
+
+        You are given positive integers n and m.
+
+        Define two integers as follows:
+
+        num1: The sum of all integers in the range [1, n] (both inclusive) that are not divisible by m.
+        num2: The sum of all integers in the range [1, n] (both inclusive) that are divisible by m.
+        Return the integer num1 - num2.
+
+
+*/
+
+
+
+
+package Leetcode_Series;
+import java.util.*;
+public class SumDifference {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a N : ");
+        int N = input.nextInt();
+        System.out.print("Enter a M : ");
+        int M = input.nextInt();
+
+        int result = differenceOfSums(N,M);
+        System.out.println(result);
+        input.close();
+    }
+    public static int differenceOfSums(int n, int m) {
+        int num1 = 0;
+        int num2 = 0;
+        for(int i = 1;i<=n;i++){
+            if(i%m==0){
+                num1 = num1 + i;
+            }
+            if(i%m!=0){
+                num2 = num2 + i;
+            }
+        }
+
+        return num2 - num1 ;
+    }
+    
+}
