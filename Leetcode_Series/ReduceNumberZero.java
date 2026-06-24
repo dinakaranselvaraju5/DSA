0aa54dc8e55d45b14fc929f9c5c1e720f4f8d143
dinakaranselvaraju5 <dinakaranselvaraju5@gmail.com>
2026-06-24 19:52:39 +0530

    Number of Steps to Reduce a Number to Zero

---
+++ Leetcode_Series/ReduceNumberZero.java
@@ -0,0 +1,56 @@
+/*
+
+    Leetcode Question Number : 1342
+    Number of Steps to Reduce a Number to Zero
+
+    Given an integer num, return the number of steps to reduce it to zero.
+    In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+    Example 1:
+
+        Input: num = 14
+        Output: 6
+        Explanation: 
+        Step 1) 14 is even; divide by 2 and obtain 7. 
+        Step 2) 7 is odd; subtract 1 and obtain 6.
+        Step 3) 6 is even; divide by 2 and obtain 3. 
+        Step 4) 3 is odd; subtract 1 and obtain 2. 
+        Step 5) 2 is even; divide by 2 and obtain 1. 
+        Step 6) 1 is odd; subtract 1 and obtain 0.
+
+
+*/
+
+package Leetcode_Series;
+
+import java.util.Scanner;
+
+public class ReduceNumberZero {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int N = input.nextInt();
+        int result = numberOfSteps(N);
+        System.out.println("The number of steps to reduce it to zero is " + result);
+        input.close();
+
+    }
+
+    public static int numberOfSteps(int num) {
+        int count = 0;
+        while(num != 0){
+            int ans = num % 2;
+            if(ans == 0){
+                num = num/2;
+            }else{
+                num = num - 1;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
+
+
