9d39709994bfaf38a762edb4156f53ac240345b5
dinakaranselvaraju5 <dinakaranselvaraju5@gmail.com>
2026-09-23 21:22:14 +0530

    --

--- Foundation/Basics/HelloWorld.java
+++ Foundation/Basics/HelloWorld.java
@@ -1,5 +1,5 @@
 
-
+package Basics;
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hellooo Worldd Modified");
--- Foundation/Basics/Main1.java
+++ Foundation/Basics/Main1.java
@@ -1,3 +1,5 @@
+package Basics;
+
 public class Main1{
     public static void main(String[] args) {
         System.out.println("Heyy Youu");
